e925bdb956b6e34cd75cd62c46afdfca07984e22
otcz <carrillozuletaoscar@gmail.com>
2022-06-22 23:00:48 -0500

    commit init

---
+++ src/test/java/spring_crudmaster/ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_crudmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

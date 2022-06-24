f8bdb402025bbc68b4b05083e498f2a16610f349
otcz <carrillozuletaoscar@gmail.com>
2022-06-24 16:47:41 -0500

    UTIL 1.2
    Todos los demas elemento

---
+++ src/main/java/com/javamaster/spring_crud/utils/Empleado.java
@@ -0,0 +1,16 @@
+package com.javamaster.spring_crud.utils;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Empleado {
+
+    @Getter
+    @Setter
+    private String email = "OSCAR";
+
+    public Empleado(String email) {
+        this.email = email;
+    }
+}

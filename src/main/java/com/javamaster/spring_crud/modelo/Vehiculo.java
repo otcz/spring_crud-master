15a6e61081b4d4a975b31da947985b916c24dd67
otcz <carrillozuletaoscar@gmail.com>
2022-06-24 08:36:50 -0500

    backend 3.5 -MODELO
    iMPORT MODELO VEHICULO

--- src/main/java/com/javamaster/spring_crud/modelo/Vehiculo.java
+++ src/main/java/com/javamaster/spring_crud/modelo/Vehiculo.java
@@ -12,12 +12,12 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+//import java.io.IOException;
+//import java.net.URI;
+//import java.net.URISyntaxException;
+//import java.net.http.HttpClient;
+//import java.net.http.HttpRequest;
+//import java.net.http.HttpResponse;
 
 @Entity
 @Table(name = "vehiculo")

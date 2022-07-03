73461e6a9e6df027ab37491e73dbeffd1a18806c
otcz <carrillozuletaoscar@gmail.com>
2022-07-02 21:38:31 -0500

    PAYU 11

--- src/test/java/com/javamaster/spring_crud/controller/VehiculoController.java
+++ src/test/java/com/javamaster/spring_crud/controller/VehiculoController.java
@@ -61,7 +61,7 @@ public class VehiculoController {
         vehiculo.setYyyvennusoat(String.valueOf((cobro.date(Calendar.YEAR) + 1)));
         vehiculo.setMmvennusoat(cobro.mes());
         vehiculo.setDdvennusoat(String.valueOf(cobro.date(Calendar.DATE)));
-        vehiculo.setCodePayU(cobro.getCodePayU());
+        vehiculo.setCodePayU("cobro.getCodePayU()");
         vehiculo.setCompro("NO");
 
         vehiculoDAO.registrar(vehiculo);

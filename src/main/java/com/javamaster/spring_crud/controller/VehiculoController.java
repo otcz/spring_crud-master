adad74bfbbc23d81533b3683936d213b459e361e
otcz <carrillozuletaoscar@gmail.com>
2022-07-05 20:08:17 -0500

    CONSULTA 4

--- src/main/java/com/javamaster/spring_crud/controller/VehiculoController.java
+++ src/main/java/com/javamaster/spring_crud/controller/VehiculoController.java
@@ -29,29 +29,31 @@ public class VehiculoController {
     @Autowired
     private Token token;
 
+    public Usuario comprador;
+
     @RequestMapping(value = "soatcolpatria.herokuapp.com/soat/vehiculo", method = RequestMethod.POST)
     public Vehiculo getUsuarios(@RequestBody Usuario comprador) {
-      // String sToken = token.obtenerToken();
-      // comprador.completarNombreUsuario(sToken);
-      // Vehiculo vehiculo = new Vehiculo();
-      // vehiculo.setPlaca(comprador.getPlaca());
-      // vehiculo.setNombres(comprador.getNombres());
-      // vehiculo.setIdentificacion(comprador.getIdentificacion());
-      // vehiculo.setTelefono(comprador.getTelefono());
-      // vehiculo.obtenerDatosVehiculoVerifik(sToken);
-      // Cobro cobro = new Cobro(vehiculo);
-      // vehiculo.setValnewsoat(cobro.calcularCobro());
-      // vehiculo.setYyycomsoat(String.valueOf(cobro.date(Calendar.YEAR)));
-      // vehiculo.setMmcomsoat(cobro.mes());
-      // vehiculo.setDdcomsoat(String.valueOf(cobro.date(Calendar.DATE)));
-      // vehiculo.setYyyvennusoat(String.valueOf((cobro.date(Calendar.YEAR) + 1)));
-      // vehiculo.setMmvennusoat(cobro.mes());
-      // vehiculo.setDdvennusoat(String.valueOf(cobro.date(Calendar.DATE)));
-      // vehiculo.setCobro(cobro.getCobro());
-      // vehiculo.setCompro("NO");
-      // vehiculoDAO.registrar(vehiculo);
-      // return vehiculo;
-
+        this.comprador = comprador;
+        // String sToken = token.obtenerToken();
+        // comprador.completarNombreUsuario(sToken);
+        // Vehiculo vehiculo = new Vehiculo();
+        // vehiculo.setPlaca(comprador.getPlaca());
+        // vehiculo.setNombres(comprador.getNombres());
+        // vehiculo.setIdentificacion(comprador.getIdentificacion());
+        // vehiculo.setTelefono(comprador.getTelefono());
+        // vehiculo.obtenerDatosVehiculoVerifik(sToken);
+        // Cobro cobro = new Cobro(vehiculo);
+        // vehiculo.setValnewsoat(cobro.calcularCobro());
+        // vehiculo.setYyycomsoat(String.valueOf(cobro.date(Calendar.YEAR)));
+        // vehiculo.setMmcomsoat(cobro.mes());
+        // vehiculo.setDdcomsoat(String.valueOf(cobro.date(Calendar.DATE)));
+        // vehiculo.setYyyvennusoat(String.valueOf((cobro.date(Calendar.YEAR) + 1)));
+        // vehiculo.setMmvennusoat(cobro.mes());
+        // vehiculo.setDdvennusoat(String.valueOf(cobro.date(Calendar.DATE)));
+        // vehiculo.setCobro(cobro.getCobro());
+        // vehiculo.setCompro("NO");
+        // vehiculoDAO.registrar(vehiculo);
+        // return vehiculo;
 
 
         Vehiculo vehiculo = new Vehiculo();
@@ -73,31 +75,65 @@ public class VehiculoController {
         vehiculo.setMmvennusoat(cobro.mes());
         vehiculo.setDdvennusoat(String.valueOf(cobro.date(Calendar.DATE)));
         vehiculo.setCompro("NO");
-      //  vehiculoDAO.registrar(vehiculo);
+        //  vehiculoDAO.registrar(vehiculo);
         return vehiculo;
     }
 
 
     //PDF
-    @RequestMapping(value = "tusoatcolpatria.com/consultas/{placa}")
-    public Vehiculo documet(HttpServletResponse response, @PathVariable String placa) {
+    @RequestMapping(value = "tusoatcolpatria.com/documentPDF", method = RequestMethod.GET)
+    public void documentPDF(HttpServletResponse response) {
         try {
-            SOAT soat = new SOAT(vehiculoDAO.buscarVehiculoPlaca(placa));
-            byte[] pdfReport = soat.generarSOAT();
-            String mimeType = "application/pdf";
-            response.setContentType(mimeType);
-            response.setHeader("Content-Disposition", String.format("attachment; filename=\"%s\"", "reporte.pdf"));
-            response.setContentLength(pdfReport.length);
-            ByteArrayInputStream inStream = new ByteArrayInputStream(pdfReport);
-            FileCopyUtils.copy(inStream, response.getOutputStream());
+            Vehiculo vehiculo = vehiculoDAO.buscarVehiculoPlaca(comprador.getPlaca());
+            if (vehiculo.getCompro().equals("NO")) {
+                SOAT soat = new SOAT(vehiculo);
+                byte[] pdfReport = soat.generarSOAT();
+                String mimeType = "application/pdf";
+                response.setContentType(mimeType);
+                response.setHeader("Content-Disposition", String.format("attachment; filename=\"%s\"", "reporte.pdf"));
+                response.setContentLength(pdfReport.length);
+                ByteArrayInputStream inStream = new ByteArrayInputStream(pdfReport);
+                FileCopyUtils.copy(inStream, response.getOutputStream());
+
+                //SE DEBE ACTUALIZAR EL CAMPO DE COMPRO
+            }
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return null;
     }
 
+
+    @RequestMapping(value = "tusoatcolpatria.com/documentPDF/{placa}")
+    public void documet(HttpServletResponse response, @PathVariable String placa) {
+        try {
+            Vehiculo vehiculo;
+            if (placa.equals("NoNumero")) {
+                vehiculo = vehiculoDAO.buscarVehiculoPlaca(comprador.getPlaca());
+            } else {
+                vehiculo = vehiculoDAO.buscarVehiculoPlaca(placa);
+            }
+            if (vehiculo.getCompro().equals("NO")) {
+                SOAT soat = new SOAT(vehiculo);
+                byte[] pdfReport = soat.generarSOAT();
+                String mimeType = "application/pdf";
+                response.setContentType(mimeType);
+                response.setHeader("Content-Disposition", String.format("attachment; filename=\"%s\"", "reporte.pdf"));
+                response.setContentLength(pdfReport.length);
+                ByteArrayInputStream inStream = new ByteArrayInputStream(pdfReport);
+                FileCopyUtils.copy(inStream, response.getOutputStream());
+
+                //SE DEBE ACTUALIZAR EL CAMPO DE COMPRO
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
     //enviarMSN y WHATSAPP
-    @RequestMapping(value = "https://soatcolpatria.herokuapp.com/api/enviar/{id}")
+    @RequestMapping(value = "tusoatcolpatria.com/consulta/enviar/{id}")
     public void enviarMSN(@PathVariable int id) {
         if (id == 1) {
             EnviarMensajeMSN mensajeMSN = new EnviarMensajeMSN("+573135331533");
@@ -111,9 +147,4 @@ public class VehiculoController {
     }
 
 
-    @RequestMapping(value = "https://tusoatcolpatria.com/consultas/{placa}")
-    public void consulta(@PathVariable String placa) {
-
-
-    }
 }
---
+++ src/main/resources/static/jss/consultar.js
@@ -0,0 +1,28 @@
+
+
+
+async function buscarDocumentPDF(){
+       try {
+       var txtPlaca= document.getElementById('_DigitalSOATQuotePortlet_Placa');
+       var s_placa="NoNumero";
+       s_placa=txtPlaca.value;
+
+       const request = await fetch('tusoatcolpatria.com/consulta/'+s_placa, {
+         method: 'POST',
+         headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/string'
+         },
+         body: s_placa;
+
+       });
+       vehiculo = await request.json();
+       else{
+           alert("ESCRIBE TU PLACA Y DESCARGA TU SOAT.");
+       }
+
+       }
+       catch (Exception){
+          alert("ESCRIBE TU PLACA Y DESCARGA TU SOAT.");
+        }
+      }

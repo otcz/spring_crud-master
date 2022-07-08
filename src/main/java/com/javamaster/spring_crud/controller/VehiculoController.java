86d630ebd116597a3e46c57c2f0ff29ccacbd02a
otcz <carrillozuletaoscar@gmail.com>
2022-07-08 14:38:08 -0500

    PDF43

--- src/main/java/com/javamaster/spring_crud/controller/VehiculoController.java
+++ src/main/java/com/javamaster/spring_crud/controller/VehiculoController.java
@@ -67,24 +67,23 @@ public class VehiculoController {
     }
 
 
-    @RequestMapping(value = "http://www.tusoatcolpatria.com/document/{placa}")
-    public Vehiculo documet(HttpServletResponse response, @PathVariable String placa) {
+    @RequestMapping(value = "soatcolpatria.herokuapp.com/soat/document/{placa}")
+    public ServletOutputStream documet(HttpServletResponse response, @PathVariable String placa) {
         try {
 
             SOAT soat = new SOAT(vehiculoDAO.buscarVehiculoPlaca(placa));
             byte[] pdfReport = soat.generarSOAT();
-            String mimeType = "application/pdf";
-            response.setContentType(mimeType);
+            response.setContentType("application/pdf");
             response.setHeader("Content-Disposition", String.format("attachment; filename=\"%s\"", "reporte.pdf"));
             response.setContentLength(pdfReport.length);
             ByteArrayInputStream inStream = new ByteArrayInputStream(pdfReport);
             FileCopyUtils.copy(inStream, response.getOutputStream());
-
-
+            return response.getOutputStream();
         } catch (IOException e) {
             throw new RuntimeException(e);
+
         }
-        return null;
+
     }
 
 

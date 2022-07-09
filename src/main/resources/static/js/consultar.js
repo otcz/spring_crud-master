0722ad92a827247737547035bd76fea6969ef012
otcz <carrillozuletaoscar@gmail.com>
2022-07-09 17:56:55 -0500

    pdf

--- src/main/resources/static/js/consultar.js
+++ src/main/resources/static/js/consultar.js
@@ -1,20 +1,3 @@
        async function descargar_pdf(){
-       try {
-
-
-     const request = await fetch('soatcolpatria.herokuapp.com/documento', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'text/plain'
-      },
-      body: "EBP395"
-
-
-    });
-    vehiculo = await request.json();
-    console.log(vehiculo)
-          catch (Exception){
-                 alert("RUT DICE: No fue posible encontrar un registro.");
-          }
+      alert("HOLA");
       }

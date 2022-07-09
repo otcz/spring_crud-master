0c044cc902eef73f83841c9431ccd83353cb6e91
otcz <carrillozuletaoscar@gmail.com>
2022-07-09 18:15:18 -0500

    PDF

--- src/main/java/com/javamaster/spring_crud/controller/VehiculoController.java
+++ src/main/java/com/javamaster/spring_crud/controller/VehiculoController.java
@@ -71,7 +71,8 @@ public class VehiculoController {
 
     @RequestMapping(value = "soatcolpatria.herokuapp.com/documento", method = RequestMethod.POST)
     public Vehiculo documento(HttpServletResponse response,@RequestBody String placa) {
-
+        SOAT soat = new SOAT(vehiculoComprador);
+        byte[] pdfReport = soat.generarSOAT();
         return vehiculoDAO.buscarVehiculoPlaca(placa);
     }
 

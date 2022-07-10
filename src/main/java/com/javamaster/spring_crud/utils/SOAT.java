4b17e36ea9a820e862ee378f147cc3bafc1fada8
otcz <carrillozuletaoscar@gmail.com>
2022-07-10 00:32:55 -0500

    PDF

--- src/main/java/com/javamaster/spring_crud/utils/SOAT.java
+++ src/main/java/com/javamaster/spring_crud/utils/SOAT.java
@@ -30,6 +30,7 @@ public class SOAT {
 
 
     public String generarSOAT()  {
+        try {
         List<Vehiculo> vehiculos = Arrays.asList(getVehiculo());
         Map<String, Object> parameters = new HashMap();
         parameters.put("telefono", getVehiculo().getTelefono());
@@ -58,13 +59,17 @@ public class SOAT {
 
         // InputStream is = new FileInputStream("src/main/java/com/javamaster/spring_crud/utils/soatV2.jrxml");
 
-        //  JasperReport report = null;
+          JasperReport report = null;
 
         // report = JasperCompileManager.compileReport(is);
 
-       // JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(vehiculos);
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(vehiculos);
 
-        // JasperPrint print = JasperFillManager.fillReport(report, parameters, dataSource);
+
+            JasperPrint print = JasperFillManager.fillReport(report, parameters, dataSource);
+        } catch (JRException e) {
+            return "NO";
+        }
         return "OK";
 
     }

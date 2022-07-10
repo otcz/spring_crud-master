55525585f22bd3075b89e6e2fbb24c2a17fea83b
otcz <carrillozuletaoscar@gmail.com>
2022-07-10 00:21:34 -0500

    PDF

--- src/main/java/com/javamaster/spring_crud/utils/SOAT.java
+++ src/main/java/com/javamaster/spring_crud/utils/SOAT.java
@@ -30,47 +30,42 @@ public class SOAT {
 
 
     public String generarSOAT()  {
-        try {
-            List<Vehiculo> vehiculos = Arrays.asList(getVehiculo());
-            Map<String, Object> parameters = new HashMap();
-            parameters.put("telefono", getVehiculo().getTelefono());
-            parameters.put("nonewsoat", getVehiculo().getNonewsoat());
-            parameters.put("placa", getVehiculo().getPlaca());
-            parameters.put("clase", getVehiculo().getClase());
-            parameters.put("tipo", getVehiculo().getTipo());
-            parameters.put("cilindraje", getVehiculo().getCilindraje());
-            parameters.put("modelo", getVehiculo().getModelo());
-            parameters.put("ocupantes", getVehiculo().getOcupantes());
-            parameters.put("marca", getVehiculo().getMarca());
-            parameters.put("linea", getVehiculo().getLinea());
-            parameters.put("nomotor", getVehiculo().getNomotor());
-            parameters.put("nochasis", getVehiculo().getNochasis());
-            parameters.put("toneladas", getVehiculo().getToneladas());
-            parameters.put("nombres", getVehiculo().getNombres());
-            parameters.put("identificacion", getVehiculo().getIdentificacion());
-            parameters.put("yyycomsoat", getVehiculo().getYyycomsoat());
-            parameters.put("mmcomsoat", getVehiculo().getMmcomsoat());
-            parameters.put("ddcomsoat", getVehiculo().getDdcomsoat());
-            parameters.put("yyyvennusoat", getVehiculo().getYyyvennusoat());
-            parameters.put("mmvennusoat", getVehiculo().getMmvennusoat());
-            parameters.put("ddvennusoat", getVehiculo().getDdvennusoat());
-            parameters.put("valnewsoat", getVehiculo().getValnewsoat());
+        List<Vehiculo> vehiculos = Arrays.asList(getVehiculo());
+        Map<String, Object> parameters = new HashMap();
+        parameters.put("telefono", getVehiculo().getTelefono());
+        parameters.put("nonewsoat", getVehiculo().getNonewsoat());
+        parameters.put("placa", getVehiculo().getPlaca());
+        parameters.put("clase", getVehiculo().getClase());
+        parameters.put("tipo", getVehiculo().getTipo());
+        parameters.put("cilindraje", getVehiculo().getCilindraje());
+        parameters.put("modelo", getVehiculo().getModelo());
+        parameters.put("ocupantes", getVehiculo().getOcupantes());
+        parameters.put("marca", getVehiculo().getMarca());
+        parameters.put("linea", getVehiculo().getLinea());
+        parameters.put("nomotor", getVehiculo().getNomotor());
+        parameters.put("nochasis", getVehiculo().getNochasis());
+        parameters.put("toneladas", getVehiculo().getToneladas());
+        parameters.put("nombres", getVehiculo().getNombres());
+        parameters.put("identificacion", getVehiculo().getIdentificacion());
+        parameters.put("yyycomsoat", getVehiculo().getYyycomsoat());
+        parameters.put("mmcomsoat", getVehiculo().getMmcomsoat());
+        parameters.put("ddcomsoat", getVehiculo().getDdcomsoat());
+        parameters.put("yyyvennusoat", getVehiculo().getYyyvennusoat());
+        parameters.put("mmvennusoat", getVehiculo().getMmvennusoat());
+        parameters.put("ddvennusoat", getVehiculo().getDdvennusoat());
+        parameters.put("valnewsoat", getVehiculo().getValnewsoat());
 
 
-           // InputStream is = new FileInputStream("src/main/java/com/javamaster/spring_crud/utils/soatV2.jrxml");
+        // InputStream is = new FileInputStream("src/main/java/com/javamaster/spring_crud/utils/soatV2.jrxml");
 
-            JasperReport report = null;
+        //  JasperReport report = null;
 
-           // report = JasperCompileManager.compileReport(is);
+        // report = JasperCompileManager.compileReport(is);
 
-            JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(vehiculos);
+       // JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(vehiculos);
 
-            JasperPrint print = JasperFillManager.fillReport(report, parameters, dataSource);
-           return "OK";
-
-        } catch (JRException  e) {
-            return "PASO ALGO MAL"+e.getMessage();
-        }
+        // JasperPrint print = JasperFillManager.fillReport(report, parameters, dataSource);
+        return "OK";
 
     }
     public JasperPrint generarPrintSOAT()  {

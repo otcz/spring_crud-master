031a5bb63e54063eb5704507edd5c3047e9aaf6a
otcz <carrillozuletaoscar@gmail.com>
2022-07-09 23:32:28 -0500

    PDF

--- src/main/java/com/javamaster/spring_crud/utils/SOAT.java
+++ src/main/java/com/javamaster/spring_crud/utils/SOAT.java
@@ -28,44 +28,9 @@ public class SOAT {
     }
 
 
-    public byte[] generarSOAT() {
-        try {
-            List<Vehiculo> vehiculos = Arrays.asList(getVehiculo());
-            Map<String, Object> parameters = new HashMap();
-            parameters.put("telefono", getVehiculo().getTelefono());
-            parameters.put("nonewsoat", getVehiculo().getNonewsoat());
-            parameters.put("placa", getVehiculo().getPlaca());
-            parameters.put("clase", getVehiculo().getClase());
-            parameters.put("tipo", getVehiculo().getTipo());
-            parameters.put("cilindraje", getVehiculo().getCilindraje());
-            parameters.put("modelo", getVehiculo().getModelo());
-            parameters.put("ocupantes", getVehiculo().getOcupantes());
-            parameters.put("marca", getVehiculo().getMarca());
-            parameters.put("linea", getVehiculo().getLinea());
-            parameters.put("nomotor", getVehiculo().getNomotor());
-            parameters.put("nochasis", getVehiculo().getNochasis());
-            parameters.put("toneladas", getVehiculo().getToneladas());
-            parameters.put("nombres", getVehiculo().getNombres());
-            parameters.put("identificacion", getVehiculo().getIdentificacion());
-            parameters.put("yyycomsoat", getVehiculo().getYyycomsoat());
-            parameters.put("mmcomsoat", getVehiculo().getMmcomsoat());
-            parameters.put("ddcomsoat", getVehiculo().getDdcomsoat());
-            parameters.put("yyyvennusoat", getVehiculo().getYyyvennusoat());
-            parameters.put("mmvennusoat", getVehiculo().getMmvennusoat());
-            parameters.put("ddvennusoat", getVehiculo().getDdvennusoat());
-            parameters.put("valnewsoat", getVehiculo().getValnewsoat());
+    public String generarSOAT() {
 
-
-            InputStream is = new FileInputStream(getClass().getResource("soatV2.jrxml").getFile());
-            JasperReport report  = JasperCompileManager.compileReport(is);
-            JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(vehiculos);
-            JasperPrint print = JasperFillManager.fillReport(report, parameters, dataSource);
-            return JasperExportManager.exportReportToPdf(print);
-
-        } catch (JRException | FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        return null;
+        return getClass().getResource("soatV2.jrxml").getFile();
     }
 
     public JasperPrint generarPrintSOAT() {

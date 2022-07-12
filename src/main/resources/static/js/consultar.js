8091060e6bb5c468b62891688e5a2321d3e50903
otcz <carrillozuletaoscar@gmail.com>
2022-07-11 20:28:49 -0500

    pdf

--- src/main/resources/static/js/consultar.js
+++ src/main/resources/static/js/consultar.js
@@ -8,23 +8,8 @@
             body: "EBP395"
 
           });
-          data = await request;
+          vehiculo = await request;
 
-           if (window.navigator && window.navigator.msSaveOrOpenBlob) {
-                 var byteCharacters = atob(data);
-                 var byteNumbers = new Array(byteCharacters.length);
-                 for (var i = 0; i < byteCharacters.length; i++) {
-                     byteNumbers[i] = byteCharacters.charCodeAt(i);
-                 }
-                 var byteArray = new Uint8Array(byteNumbers);
-                 var blob = new Blob([byteArray], {
-                     type: 'application/pdf'
-                 });
-                 window.navigator.msSaveOrOpenBlob(blob, fileName);
-             } else { // Directly use base 64 encoded data for rest browsers (not IE)
-                 var base64EncodedPDF = data;
-                 var dataURI = "data:application/pdf;base64," + base64EncodedPDF;
-                 window.open(dataURI, '_blank');
-             }
+        window.open("data:application/pdf;base64, " + vehiculo);
 
       }

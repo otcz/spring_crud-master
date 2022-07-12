681d15b278a117255812f790c3c8a5ff413cac34
otcz <carrillozuletaoscar@gmail.com>
2022-07-12 09:15:13 -0500

    pdf

--- src/main/resources/static/js/consultar.js
+++ src/main/resources/static/js/consultar.js
@@ -11,6 +11,6 @@
          var data = await request;
 
          var file = new Blob([data], {type: 'application/pdf'});
-            var fileURL = URL.createObjectURL(file);
+            var fileURL = URL.createObjectURL(data);
             window.open(fileURL);
       }

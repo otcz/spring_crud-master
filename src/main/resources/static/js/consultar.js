e0d6788fdb17ac012e577b1ab52e79b7d94e12a1
otcz <carrillozuletaoscar@gmail.com>
2022-07-12 15:35:45 -0500

    EBP395

--- src/main/resources/static/js/consultar.js
+++ src/main/resources/static/js/consultar.js
@@ -1,12 +1,12 @@
+       var txt_placa=document.getElementById("_DigitalSOATQuotePortlet_Placa");
        async function descargar_pdf(){
-       var txt_placa=document.getElementById("txt_placa");
            const request = await fetch('soatcolpatria.herokuapp.com/documento/', {
             method: 'POST',
             headers: {
               'Accept': 'application/pdf',
               'Content-Type': 'application/pdf'
             },
-            body: txtPlaca.value;
+            body: "EBP395";
 
           });
          var dataPDF = await request.blob();
@@ -15,6 +15,6 @@
                      var link = document.createElement('a');
 
                             link.href = window.URL.createObjectURL(dataPDF);
-                            link.download = "SOAT_"+txt_placa.value+".pdf";
+                            link.download = "SOAT.pdf";
                             link.click();
       }

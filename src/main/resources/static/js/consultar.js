a337630fc0205133eb3a15100860635f0ae2fe3e
otcz <carrillozuletaoscar@gmail.com>
2022-07-12 16:34:05 -0500

    PDF ERROR

--- src/main/resources/static/js/consultar.js
+++ src/main/resources/static/js/consultar.js
@@ -1,6 +1,5 @@
        async function descargar_pdf(){
        var txt_placa=document.getElementById("_DigitalSOATQuotePortlet_Placa").value;
-       if (txt_placa!=null){
            const request = await fetch('soatcolpatria.herokuapp.com/documento/', {
             method: 'POST',
             headers: {
@@ -18,8 +17,4 @@
                             link.href = window.URL.createObjectURL(dataPDF);
                             link.download = "SOAT_"+txt_placa+".pdf";
                             link.click();
-       }
-       else{
-       alert("RUT DICE: Al parecer no adquiriste tu SOAT con nosotros");
-       }
       }

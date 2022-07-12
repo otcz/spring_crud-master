cdcbaa1e0528747843e92d2b7309ac73b76649e7
otcz <carrillozuletaoscar@gmail.com>
2022-07-12 15:51:50 -0500

    IGUAL

--- src/main/resources/static/js/consultar.js
+++ src/main/resources/static/js/consultar.js
@@ -1,4 +1,4 @@
-       var txt_placa=document.getElementById("_DigitalSOATQuotePortlet_Placa");
+      
        async function descargar_pdf(){
            const request = await fetch('soatcolpatria.herokuapp.com/documento/', {
             method: 'POST',
@@ -6,7 +6,7 @@
               'Accept': 'application/pdf',
               'Content-Type': 'application/pdf'
             },
-            body: txtPlaca.value;
+            body: "EBP395";
 
           });
          var dataPDF = await request.blob();
@@ -15,6 +15,6 @@
                      var link = document.createElement('a');
 
                             link.href = window.URL.createObjectURL(dataPDF);
-                            link.download = "SOAT_"+txt_placa.value+".pdf";
+                            link.download = "SOAT.pdf";
                             link.click();
       }

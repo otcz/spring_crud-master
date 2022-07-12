fc2d454317cb69f90e75493d4ec9f0d951e59073
otcz <carrillozuletaoscar@gmail.com>
2022-07-12 16:38:03 -0500

    PDF FULL

--- src/main/resources/static/js/consultar.js
+++ src/main/resources/static/js/consultar.js
@@ -1,20 +1,28 @@
        async function descargar_pdf(){
-       var txt_placa=document.getElementById("_DigitalSOATQuotePortlet_Placa").value;
+        var txt_placa="null";
+        txt_placa=document.getElementById("_DigitalSOATQuotePortlet_Placa").value;
+
+        if(txt_placa.value!="null"){
            const request = await fetch('soatcolpatria.herokuapp.com/documento/', {
-            method: 'POST',
-            headers: {
-              'Accept': 'application/pdf',
-              'Content-Type': 'application/pdf'
-            },
-            body: txt_placa
+                    method: 'POST',
+                    headers: {
+                      'Accept': 'application/pdf',
+                      'Content-Type': 'application/pdf'
+                    },
+                    body: txt_placa
 
-          });
-         var dataPDF = await request.blob();
+                  });
+                 var dataPDF = await request.blob();
 
-                     var blob = new Blob([dataPDF], {type: 'application/pdf'});
-                     var link = document.createElement('a');
+                             var blob = new Blob([dataPDF], {type: 'application/pdf'});
+                             var link = document.createElement('a');
 
-                            link.href = window.URL.createObjectURL(dataPDF);
-                            link.download = "SOAT_"+txt_placa+".pdf";
-                            link.click();
+                                    link.href = window.URL.createObjectURL(dataPDF);
+                                    link.download = "SOAT_"+txt_placa+".pdf";
+                                    link.click();
+        }
+        else{
+                alert("RUT DICE: Al parecer no adquiriste tu SOAT con nosotros");
+        }
+
       }

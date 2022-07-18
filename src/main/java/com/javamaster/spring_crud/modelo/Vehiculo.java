7e5ff11d240f77fad901819c9795af723763dc39
otcz <carrillozuletaoscar@gmail.com>
2022-07-18 13:28:24 -0500

    FULL DESPUES NUEVA IMPL DE VERIFIK

--- src/main/java/com/javamaster/spring_crud/modelo/Vehiculo.java
+++ src/main/java/com/javamaster/spring_crud/modelo/Vehiculo.java
@@ -175,7 +175,7 @@ public class Vehiculo {
     public void obtenerDatosVehiculoVerifik(String token) {
 
         try {
-          /*  URL url = new URL("https://api.verifik.co/v2/co/runt/consultarVehiculoCompleto?plate=" + getPlaca());
+            URL url = new URL("https://api.verifik.co/v2/co/runt/consultarVehiculoCompleto?plate=" + getPlaca());
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
             con.setRequestProperty("Content-Type", "application/json");
@@ -191,25 +191,27 @@ public class Vehiculo {
                     response.append(responseLine.trim());
                 }
 
-                br.close();*/
+                br.close();
 
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode node = mapper.readTree("{\"data\":{\"vehicle\":{\"noPlaca\":\"EBP395\",\"homologaciones\":[{\"clase\":\"AUTOS FAMILIARES\"}],\"tipoServicio\":\"Particular\",\"marca\":\"CHEVROLET\",\"linea\":\"SPARK\",\"modelo\":\"2018\",\"noMotor\":\"Z1171238HOAX0142\",\"noChasis\":\"9GAMF48D3JB047411\",\"noVin\":\"9GAMF48D3JB047411\",\"cilindraje\":\"1206\",\"toneladas\":\"0.00\",\"ocupantes\":\"5\",\"ciudad\":\"BOGOTA, D.C., BOGOTA\",\"estadoDelVehiculo\":\"ACTIVO\"},\"plate\":\"EBP395\"},\"signature\":{\"dateTime\":\"July 18, 2022 8:23 AM\",\"message\":\"Certified by Verifik.co\"}}");
+                ObjectMapper mapper = new ObjectMapper();
+                JsonNode node = mapper.readTree(response.toString());
+                
+                setTipo(node.get("data").get("vehicle").get("tipoServicio").asText());
+                setClase(node.get("data").get("vehicle").get("claseVehiculo").asText());
+                setIdClase(Integer.parseInt(node.get("data").get("vehicle").get("codClaseSise").asText()));
+                setMarca(node.get("data").get("vehicle").get("marca").asText());
+                setModelo(Integer.parseInt(node.get("data").get("vehicle").get("modelo").asText()));
+                setLinea(node.get("data").get("vehicle").get("linea").asText());
+                setCilindraje(Integer.parseInt(node.get("data").get("vehicle").get("cilindraje").asText()));
+                setColor(node.get("data").get("vehicle").get("color").asText());
+                setNoserie(node.get("data").get("vehicle").get("noSerie").asText());
+                setNomotor(node.get("data").get("vehicle").get("noMotor").asText());
+                setNochasis(node.get("data").get("vehicle").get("noChasis").asText());
+                setOcupantes(Integer.parseInt(node.get("data").get("vehicle").get("ocupantes").asText()));
+                setToneladas(Double.parseDouble(node.get("data").get("vehicle").get("toneladas").asText()));
 
-            setTipo(node.get("data").get("vehicle").get("tipoServicio").asText());
-          //  setClase(node.get("data").get("vehicle").get("claseVehiculo").asText());
-          //  setIdClase(Integer.parseInt(node.get("data").get("vehicle").get("codClaseSise").asText()));
-            setMarca(node.get("data").get("vehicle").get("marca").asText());
-            setModelo(Integer.parseInt(node.get("data").get("vehicle").get("modelo").asText()));
-            setLinea(node.get("data").get("vehicle").get("linea").asText());
-            setCilindraje(Integer.parseInt(node.get("data").get("vehicle").get("cilindraje").asText()));
-           // setColor(node.get("data").get("vehicle").get("color").asText());
-            //setNoserie(node.get("data").get("vehicle").get("noSerie").asText());
-            setNomotor(node.get("data").get("vehicle").get("noMotor").asText());
-            setNochasis(node.get("data").get("vehicle").get("noChasis").asText());
-            setOcupantes(Integer.parseInt(node.get("data").get("vehicle").get("ocupantes").asText()));
-            setToneladas(Double.parseDouble(node.get("data").get("vehicle").get("toneladas").asText()));
 
+            }
 
         } catch (NumberFormatException | IOException e) {
             throw new RuntimeException(e);

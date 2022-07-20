6953db5869bb12c92e575b051e5b256ac12be468
otcz <carrillozuletaoscar@gmail.com>
2022-07-19 19:44:27 -0500

    setCostoTotal

--- src/main/java/com/javamaster/spring_crud/modelo/Vehiculo.java
+++ src/main/java/com/javamaster/spring_crud/modelo/Vehiculo.java
@@ -252,8 +252,8 @@ public class Vehiculo {
             // setCodigoTarifa(node.get("data").get("vehiculo").get("codigoTarifa").asText());
             // setPrima((node.get("data").get("vehiculo").get("prima").asText()));
             // setContribucion((node.get("data").get("vehiculo").get("contribucion").asText()));
-             setRunt((node.get("data").get("vehiculo").get("rut").asText()));
-            // setCostoTotal(node.get("data").get("vehiculo").get("costoTotal").asText());
+            // setRunt((node.get("data").get("vehiculo").get("rut").asText()));
+            setCostoTotal(node.get("data").get("vehiculo").get("costoTotal").asText());
 
 
         } catch (NumberFormatException | IOException e) {

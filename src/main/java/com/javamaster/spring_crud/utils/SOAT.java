e7803064df17ac2c3c314851828d62fb8332123d
otcz <carrillozuletaoscar@gmail.com>
2022-07-23 16:10:04 -0500

    SOAT

--- src/main/java/com/javamaster/spring_crud/utils/SOAT.java
+++ src/main/java/com/javamaster/spring_crud/utils/SOAT.java
@@ -55,10 +55,10 @@ public class SOAT {
             parameters.put("mmvennusoat", getVehiculo().getMmvennusoat());
             parameters.put("ddvennusoat", getVehiculo().getDdvennusoat());
             parameters.put("codigotarifa", getVehiculo().getCodigotarifa());
-            parameters.put("costototal", (getVehiculo().getCostototal()));
-            parameters.put("prima", (getVehiculo().getPrima()));
-            parameters.put("contribucion", (getVehiculo().getContribucion()));
-            parameters.put("runt", (getVehiculo().getRunt()));
+            parameters.put("costototal", dollarFormat.format(Double.parseDouble(getVehiculo().getCostototal())));
+            parameters.put("prima", dollarFormat.format(Double.parseDouble(getVehiculo().getPrima())));
+            parameters.put("contribucion", dollarFormat.format(Double.parseDouble(getVehiculo().getContribucion())));
+            parameters.put("runt", dollarFormat.format(Double.parseDouble(getVehiculo().getRunt())));
 
 
             InputStream is = new FileInputStream("src/main/java/com/javamaster/spring_crud/utils/soatV2.jrxml");
